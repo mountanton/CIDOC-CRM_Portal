7b8e3277391445c8b52cc23a6bc27eb0b343200a
Michalis Mountantonakis <mountanton@csd.uoc.gr>
2025-10-23 00:50:41 +0300

    Update backend-url.ts

--- CIDOC-CRM_Portal_Frontend/src/app/core/services/backend-url.ts
+++ CIDOC-CRM_Portal_Frontend/src/app/core/services/backend-url.ts
@@ -1,2 +1,2 @@
-export const url = "http://localhost:8080/";
-// export const url = "http://83.212.101.188:8081/CIDOC_API-1.0-SNAPSHOT/portal/cidoc/";
+//export const url = "http://localhost:8080/";
+export const url = "https://demos.isl.ics.forth.gr/CIDOC_API/portal/cidoc/";
